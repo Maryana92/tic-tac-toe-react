a9ccb8c0c07bfea94c6361d62955915d15a9bcf5
Andreas_Just <andreas.wq@gmail.com>
2019-02-21 18:51:54 +0200

    Updates

---
+++ precache-manifest.be0779e90f9e7537058ad7771e4d0be9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "aea081033a4dd01f4e2f",
+    "url": "/static/js/main.aea08103.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "aea081033a4dd01f4e2f",
+    "url": "/static/css/main.47b55005.chunk.css"
+  },
+  {
+    "revision": "4f8fe4de3a5a0473ba0c1893fd6f4b13",
+    "url": "/index.html"
+  }
+];
